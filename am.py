abb30600cfd294c3ea5b1dc972cff4f7fd5b717c
VSaiAmruth <78521262+VSaiAmruth@users.noreply.github.com>
2021-02-04 09:40:53 +0530

    Create am.py

---
+++ am.py
@@ -0,0 +1,15 @@
+
+print("my name is Amruth")
+list=[1,2,3,4]
+print(list)
+print("Amruth")
+print("KLH HYDERABAD")
+print("kl university")
+print("Sai amruth")
+
+
+
+
+for i in range(0,10):
+  print(i)
+print("Hello word")
